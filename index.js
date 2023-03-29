a95268578f776cdea11b7d0d3fdf0f9ce69dbb8e
Anthony Lotson <focus_and_finish@yahoo.com>
2023-03-29 02:06:23 -0400

    lab completed

---
+++ index.js
@@ -0,0 +1,27 @@
+/*const divide = function(){
+};
+*/
+
+let a = 2000;
+let b = 100;
+const divide = function(a, b){
+       return a/b;
+};
+// could define a and b after the function declaration as well
+//let a = 2000;
+//let b = 100;
+
+
+//const square = () => {
+//};
+
+//const square = () => x**;
+
+//const square = () => x**2;
+//x;
+
+const square = x => x**2;
+//x;
+
+const add = (a, b) => a + b;
+//x;
